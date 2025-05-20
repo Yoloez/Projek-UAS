51220656a383825468d3c30e3ec81929b1c04802
hanan <hananfijananto@gmail.com>
2025-05-20 21:16:10 +0700

    foto-bg

--- authentication/DAFTAR/daftar.php
+++ authentication/DAFTAR/daftar.php
@@ -1,12 +1,14 @@
 <?php
 $servername = "localhost";
-$database = "db_pertanian"
 $username = "root";
-$password = ""
+$password = "";
+$database = "db_pertanian";
 
-$conn = mysqli_connect($servername, $username, $password, $database)
-if (!$conn) {
+$conn = mysqli_connect($servername, $username, $password, $database);
+if(!$conn){
     die("Koneksi Gagal : " . mysqli_connect_error());
 } else {
     echo "Koneksi Berhasil";
 }
+
+?>
