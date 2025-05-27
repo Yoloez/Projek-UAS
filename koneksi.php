e743bddc2bcb8532e7c7613ec6385546cfeb2278
hanan <hananfijananto@gmail.com>
2025-05-28 06:52:54 +0700

    koneksi

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "uas";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error) {
+    die("Koneksi gagal: " . $conn ->
+    connect_error);
+}
+echo "Koneksi Berhasil";
+
+?>
